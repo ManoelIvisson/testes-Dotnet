1874525eea7ac1a215dd7da029b382e2dd946864
Manoel Ívisson <manoelivisson4@hotmail.com>
2026-05-06 16:59:22 -0300

    Adicionando terceiro exercício

---
+++ gritado/Program.cs
@@ -0,0 +1,21 @@
+﻿static string seAcalmando(string grito)
+{
+  int s = 0;
+
+    if (grito.Count(c => c == '?') > 1)
+    {
+      int primeiraInterrogacao = grito.IndexOf("?");
+      Console.WriteLine(primeiraInterrogacao);
+      grito = grito.Replace("?", "!");
+      grito = grito.Insert(primeiraInterrogacao, "?").Trim();
+      int primeiraExclamacao = grito.IndexOf("!");
+      Console.WriteLine(primeiraExclamacao);
+      grito = grito.Replace("!", "");
+      grito = grito.Insert(primeiraExclamacao, "!").Trim();
+    }
+  return grito;
+}
+
+Console.WriteLine("Grite!");
+string grito = Console.ReadLine();
+Console.WriteLine(seAcalmando(grito));
